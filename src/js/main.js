75b100b3934fd5054ef485ab49a88035708f367f
Jared Neems <jared.neems@stats.govt.nz>
2016-10-18 14:44:41 +1300

    first

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+// All Imports
+//jQuery
+$ = jQuery = require('jquery');
+
+//SlideOut Nav
+var Slideout = require('slideout');
+
+//Materialize js
+var materialize = require("materialize");
+
+//Custom js
+var customjs = require('./custom.js');
+
+//ALL CUSTOM JS
+/*
+Created: Sep 30, 2016 
+Author:  Jared Neems // Statistics New Zealand
+Pupose:  Front End UI Javascript For Initial Design, User Interface, 
+		 Responsive Testing and Presentation.
+Standards: 
+*/
+
+customjs.cm();
+
+
+
+
+
+
+
+
+
+
+
+
