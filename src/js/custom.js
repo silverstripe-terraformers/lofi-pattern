b1a746e378276cc05faad1e549df5c648573d6aa
Jared Neems <jared.neems@stats.govt.nz>
2016-10-25 16:41:22 +1300

    thisisnew

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,7 +10,7 @@ exports.cm = function(){
 
 (function(){
 		
-		// Initiale namespace instance to creeate accesible object
+		// Initiale namespace instance to create accesible object
 		UI = namespace.nm();
 
 		//Use namespace method
@@ -21,41 +21,15 @@ exports.cm = function(){
 
 	 		//Check window size and run internal functions. 
 	 		var checkSize = $(window).width();
-	 		var oldNav = 'leftNavContainer';
-	 		var newBody = 'cardWrapper';
 
 	 		if(checkSize <= 1180){
-	 			removeNav();
-	 			stretchHorizontalNav();
-	 			updateMatClass();
+	 			mobileSideNav();
 	 		}
-
-	 		//Removes static desktop side nav
-			function removeNav(){
-				//Grab the black side nav and remove it from the dom.
-				var elem = document.getElementById(oldNav);
-				elem.parentNode.removeChild(elem);
-			}
-
-			//Changes Material design class to full width
-			function updateMatClass(){
-				//Change Materilize layout from 10 to 12 for responsiv mobile. Checking via regex.
-				document.getElementById(newBody).className = //cont' to next line
-				document.getElementById(newBody).className.replace( /(?:^|\s)s10(?!\S)/g , ' s12' );
-				//Trigger inclusion of mobile side nav
-				mobileSideNav();
-			}
-
-			//Stretches current horizontal nav to full width after side nav removal
-			function stretchHorizontalNav(){
-				document.getElementById("navHorizontal").style.width = "100%";
-			}
 
 			//Activate and setup mobile side navigation
 			function mobileSideNav(){
 				$('.button-collapse').sideNav();
 				$('.side-nav').css('display', 'block');
-				$('#mobMenu').css('display', 'inline-block');
 			}
 
 		};
@@ -83,6 +57,7 @@ exports.cm = function(){
 	// Monitor Screen size for new menu breakpoint:
 	// UI.menus();	
 	UI.init();
+	UI.menus();
 
 })(); //iffe ends
 
